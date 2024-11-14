234c37171052099cbf5fcfe496b01507a8ac0f69
Ouyang <shuaiouyang@mail.sdu.edu.cn>
2024-11-14 13:37:35 +0800

    init the pcie readout by YangHaoyan

---
+++ pcie_control/tools/main.h
@@ -0,0 +1,13 @@
+//
+// Created by tao on 23-9-1.
+//
+
+#include <semaphore.h>
+#include <pthread.h>
+
+#ifndef PCIE_CONTROL_MAIN_H
+#define PCIE_CONTROL_MAIN_H
+
+#endif //PCIE_CONTROL_MAIN_H
+
+int reg_rw(int argc, char **argv,int fdi);
