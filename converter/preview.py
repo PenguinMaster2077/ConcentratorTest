4b3a61185ee83cd7891b5c818ca7c71b13d158af
greatofdream <greatofdream@gmail.com>
2024-11-15 17:02:35 +0800

    add new range for the preview

--- converter/preview.py
+++ converter/preview.py
@@ -3,7 +3,8 @@ preview the data
 '''
 import h5py
 def loadH5(f):
-    with h5py.File(f) as ipt:
+    print(f'load file {f}')
+    with h5py.File(f, 'r') as ipt:
         waves = ipt['Readout/Waveform'][:]
     return waves
 import argparse
@@ -22,6 +23,7 @@ args = psr.parse_args()
 waves = np.concatenate([loadH5(f) for f in args.ipt])
 N_ch = args.N_ch
 
+# ch1 positive; ch2 negative
 if args.onlywave:
     with PdfPages(args.opt) as pdf:
         for i, w in enumerate(waves[:args.N]):
@@ -34,14 +36,31 @@ if args.onlywave:
             ax.legend()
             pdf.savefig(fig)
             plt.close()
+        fig, ax = plt.subplots()
+        for i, w in enumerate(waves[:args.Ns]):
+            ax.plot(w[0])
+        ax.set_xlabel('t')
+        ax.set_ylabel('ADC')
+        pdf.savefig(fig)
+        plt.close()
+        fig, ax = plt.subplots()
+        for i, w in enumerate(waves[:args.Ns]):
+            ax.plot(w[1])
+        ax.set_xlabel('t')
+        ax.set_ylabel('ADC')
+        pdf.savefig(fig)
+        plt.close()
     exit(0)
 res = np.empty((waves.shape[0], N_ch), dtype=[('eid', np.int32), ('Q', np.float64), ('baseline', np.float64), ('peak', np.float64), ('peakPos', np.int32)])
 
-# omit the first 200ns, which is used as baseline
-pre_l, w_l, w_r = 200, 20, 70
+# omit the first 100ns, which is used as baseline
+pre_l, w_l, w_r = 100, 20, 70
 for i, ws in enumerate(waves):
     for j in range(N_ch):
-        w = ws[j]
+        if j==1:
+            w = -ws[j]
+        else:
+            w = ws[j]
         baseline = np.mean(w[:pre_l])
         peakPos = np.argmin(w[pre_l:]) + pre_l
         peak = baseline - w[peakPos]
@@ -50,22 +69,53 @@ for i, ws in enumerate(waves):
 with PdfPages(args.opt) as pdf:
     fig, ax = plt.subplots()
     for j in range(N_ch):
-        ax.hist(res[:, j]['peakPos'](res[:, j]['peak']>30), bins=1000, range=[0, 1000], histtype='step')
+        ax.hist(res[:, j]['peakPos'][res[:, j]['peak']>30], bins=1000, range=[0, 1000], histtype='step', label=f'ch{j}')
+    ax.set_yscale('log')
     ax.set_xlabel('t')
-    ax.set_ylable('entries')
+    ax.set_ylabel('entries')
+    ax.legend()
     pdf.savefig(fig)
 
     fig, ax = plt.subplots()
     for j in range(N_ch):
-        ax.hist(res[:, j]['Q'], bins=1000, histtype='step')
+        ax.hist(res[:, j]['peakPos'][res[:, j]['peak']>30], bins=300, range=[120, 920], histtype='step', label=f'ch{j}')
+    ax.set_yscale('log')
+    ax.set_xlabel('t')
+    ax.set_ylabel('entries')
+    ax.legend()
+    pdf.savefig(fig)
+
+    fig, ax = plt.subplots()
+    for j in range(N_ch):
+        ax.hist(res[:, j]['Q'], bins=1000, histtype='step', label=f'ch{j}')
     ax.set_xlabel('charge')
-    ax.set_ylable('entries')
+    ax.set_ylabel('entries')
+    ax.set_yscale('log')
+    ax.legend()
     pdf.savefig(fig)
 
     fig, ax = plt.subplots()
     for j in range(N_ch):
-        ax.hist(res[:, j]['peak'], bins=1000, histtype='step')
+        ax.hist(res[:, j]['peak'], bins=1000, histtype='step', label=f'ch{j}')
     ax.set_xlabel('peak')
-    ax.set_ylable('entries')
+    ax.set_ylabel('entries')
+    ax.set_yscale('log')
+    ax.legend()
+    pdf.savefig(fig)
+
+    fig, ax = plt.subplots()
+    ax.hist(res[:, 0]['baseline'], bins=1000, histtype='step', label='ch0')
+    ax.set_yscale('log')
+    ax.set_xlabel('baseline')
+    ax.set_ylabel('entries')
+    ax.legend()
+    pdf.savefig(fig)
+
+    fig, ax = plt.subplots()
+    ax.hist(res[:, 1]['baseline'], bins=1000, histtype='step', label='ch1')
+    ax.set_yscale('log')
+    ax.set_xlabel('baseline')
+    ax.set_ylabel('entries')
+    ax.legend()
     pdf.savefig(fig)
 
